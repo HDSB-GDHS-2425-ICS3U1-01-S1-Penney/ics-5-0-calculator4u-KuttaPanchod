af41cb64468c5a38796ab3cb138ecbd14a951e35
KuttaPanchod <1jeorsuk@hdsb.ca>
2024-09-12 12:39:08 -0400

    Update index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+console.log("Welcome to Super Calculator! Use + to add, - to subtract, * to multiply, and / to divide.");
+x = prompt("First Number?", x);
+console.log(x);
+
+
+function add(x, y) {
+    
+}
+function subtract(x, y) {
+    
+}
+function multiply(x, y) {
+    
+}
+function divide(x, y) {
+    
+}
